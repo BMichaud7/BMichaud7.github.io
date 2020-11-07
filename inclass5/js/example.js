2b1c3ce612c0fe389ae81f8c7833ebf64540954a
BMichaud7 <brendanmichaud7@gmail.com>
2020-11-07 16:00:00 -0500

    Done

--- inclass5/js/example.js
+++ inclass5/js/example.js
@@ -6,34 +6,35 @@
 //  copied or excerpted for educational purposes with credit to the
 // author.
 //  updated by BM on 11-05-2020 at 4:30 pm
+    // Github: https://bmichaud7.github.io/Inclass5/example.html
 // -->
 
 
-var list = document.getElementsByTagName('ul')[0]; //make var that is list i.e. getElementsByTagName('ul')[0]
+var list = document.getElementsByTagName("ul")[0]; //make var that is list i.e. getElementsByTagName("ul")[0]
 // ADD NEW ITEM TO END OF LIST
-var newLast = document.createElement('li');
-var newLastName = document.createTextNode('cream');
+var newLast = document.createElement("li");
+var newLastName = document.createTextNode("cream");
 newLast.appendChild(newLastName);
 list.appendChild(newLast);
 
 // ADD NEW ITEM START OF LIST
-var newFirst = document.createElement('li');
-var newFirstName = document.createTextNode('kale');
+var newFirst = document.createElement("li");
+var newFirstName = document.createTextNode("kale");
 newFirst.appendChild(newFirstName);
 list.insertBefore(newFirst, list.firstChild);
 
 // ADD A CLASS OF COOL TO ALL LIST ITEMS
-var itemsInList = document.querySelectorAll('li');
+var itemsInList = document.querySelectorAll("li");
 var i;
 for (i = 0; i < itemsInList.length; i++) {
-  itemsInList[i].className = 'cool';
+  itemsInList[i].className = "cool";
 }
 
 // ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
 
-var heading = document.querySelector('h2'); 
+var heading = document.querySelector("h2"); 
 var headingName = heading.firstChild.nodeValue;
 var total = itemsInList.length; 
 
-var headingNew = headingName + '<span>' + total + '</span>';
+var headingNew = headingName + "<span>" + total + "</span>";
 heading.innerHTML = headingNew;
