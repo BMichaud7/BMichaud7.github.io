10442f925082e3333d77fc603bccf035ede906ea
BMichaud7 <brendanmichaud7@gmail.com>
2020-12-17 16:37:32 -0500

    HW8

--- HW8/js/script.js
+++ HW8/js/script.js
@@ -11,190 +11,6 @@
       freely copied or excerpted for educational purposes with credit to the author.
       -->
 */
-
-var ScrabbleTiles = [];
-//holds info of scrabble tiles
-var IMAGES = {
-  A: "Scrabble_Tile_A.jpg",
-  B: "Scrabble_Tile_B.jpg",
-  C: "Scrabble_Tile_C.jpg",
-  D: "Scrabble_Tile_D.jpg",
-  E: "Scrabble_Tile_E.jpg",
-  F: "Scrabble_Tile_F.jpg",
-  G: "Scrabble_Tile_G.jpg",
-  H: "Scrabble_Tile_H.jpg",
-  I: "Scrabble_Tile_I.jpg",
-  J: "Scrabble_Tile_J.jpg",
-  K: "Scrabble_Tile_K.jpg",
-  L: "Scrabble_Tile_L.jpg",
-  M: "Scrabble_Tile_M.jpg",
-  N: "Scrabble_Tile_N.jpg",
-  O: "Scrabble_Tile_O.jpg",
-  P: "Scrabble_Tile_P.jpg",
-  Q: "Scrabble_Tile_Q.jpg",
-  R: "Scrabble_Tile_R.jpg",
-  S: "Scrabble_Tile_S.jpg",
-  T: "Scrabble_Tile_T.jpg",
-  U: "Scrabble_Tile_U.jpg",
-  V: "Scrabble_Tile_V.jpg",
-  W: "Scrabble_Tile_W.jpg",
-  X: "Scrabble_Tile_X.jpg",
-  Y: "Scrabble_Tile_Y.jpg",
-  Z: "Scrabble_Tile_Z.jpg",
-  _: "Scrabble_Tile_Blank.jpg",
-};
-
-// dictionary
-var dictarray = {};
-
-$(function () {
-  ButtonListeners();
-
-  //adding event listener for buttons
-  Setup();
-
-  //setting up scrabble the game
-  DragAndDrop();
-
-  //setting up drag and drop
-  Dictionary();
-
-  //setting up the dictonary for checking if current word is a word
-});
-
-// sets up the event listeners for buttons
-
-function ButtonListeners() {
-  //event listeners for the buttons
-  $("#submitButton").click(function () {
-    SubmitClick();
-  });
-
-  $("#resetButton").click(function () {
-    ResetAllLetters();
-  });
-
-  $("#newGame").click(function () {
-    NewGame();
-  });
-}
-
-//  dictionary to array
-
-
-//https://johnresig.com/blog/dictionary-lookups-in-javascript/
-function Dictionary() {
-  //request the text dictionary
-  $.get(
-    "https:raw.githubusercontent.com/BMichaud7/BMichaud7.github.io/master/dict.txt",
-    function (txt, status) {
-      if (status == "success") {
-        // Get an array of all the words
-        var words = txt.split("\n");
-
-        //add word to dict
-        for (var i = 0; i < words.length; i++) {
-          dictarray[words[i]] = true;
-        }
-      }
-    }
-  );
-}
-
-// sets up drag and drop 
-//https://api.jqueryui.com/droppable/#event-drop
-function DragAndDrop() {
-  $(".selector").draggable({
-    delay: 100,
-    containment: "body",
-    revert: "invalid",
-
-    //reverts to hand if not placed on board
-    stack: ".selector",
-
-    //keeps draggable on top of all of the elements
-    drag: function (event, ui) {
-      if (
-        $(this).parent().attr("name") == "_" &&
-        $(this).parent().parent().attr("id") == "board"
-      ) {
-        $(this).parent().removeAttr("name");
-        $(this).attr("id", "_");
-        $(this).attr("src", "Scrabble_Tiles/" + GetImage("_"));
-      }
-    },
-  });
-//https://api.jqueryui.com/droppable/#event-drop
-  $(".spot").droppable({
-    //droppable event for the spots on board
-    tolerance: "intersect",
-    drop: function (event, ui) {
-      //drop event
-      $(ui.draggable)
-        .detach()
-        .css({ top: 0, left: 0 })
-        .appendTo("#" + this.id);
-      $("#" + this.id).droppable("disable");
-      MakespotsDroppable();
-      //resets spots on board that have no tiles and are no longer droppable
-      if (ui.draggable.attr("id") == "_") {
-        PromptFor_(ui.draggable);
-      }
-    },
-  });
-//https://api.jqueryui.com/droppable/#event-drop
-  $("#hand").droppable({
-    //making hand droppable
-    drop: function (event, ui) {
-      $(ui.draggable)
-        .detach()
-        .css({ top: 0, left: 0 })
-        .appendTo("#" + ui.draggable.attr("name"));
-      //code from Barry Pitman on stackoverflow.
-      MakespotsDroppable();
-    },
-  });
-}
-
-// Prompts the user letter thay want to use for a _ tile. Keeps asking until user enters A-Z
-function PromptFor_(tile) {
-  do {
-    tileRequested = prompt("Enter a letter for the _ Title");
-
-    if (
-      tileRequested != null &&
-      tileRequested.length == 1 &&
-      tileRequested.match(/[a-zA-Z]+/g)
-    ) {
-      tileRequested = tileRequested.toUpperCase();
-      $(tile).parent().attr("name", "_");
-      //used for resetting tiles
-      $(tile).attr("src", "Scrabble_Tiles/" + GetImage(tileRequested.trim()));
-      $(tile).attr("id", tileRequested);
-      //changing _ tile to requested tile
-    }
-  } while (
-    tileRequested == null ||
-    tileRequested.length > 1 ||
-    tileRequested.match(/[a-zA-Z]+/g) == null
-  );
-
-  //looping while user does not enter a valid response. This means the user needs to enter just A-Z
-}
-
-// removes _ tile from board
-function Remove_() {
-  $("#board div").each(function (index) {
-    //looping through each div in #board div
-    if ($(this).attr("name")) {
-      //checking if div has a name attribute
-      $(this).removeAttr("name");
-      //removing attribute from div
-    }
-  });
-}
-
-// Sets up ScrabbleTiles object and gets tles to add to the hand
 function Setup() {
   //code from Jesse Heines Scrabble_Pieces_AssociativeArray_Jesse.js
   ScrabbleTiles["A"] = {
@@ -336,6 +152,190 @@ function Setup() {
   GetTiles();
 }
 
+var ScrabbleTiles = [];
+//holds info of scrabble tiles
+var IMAGES = {
+  A: "Scrabble_Tile_A.jpg",
+  B: "Scrabble_Tile_B.jpg",
+  C: "Scrabble_Tile_C.jpg",
+  D: "Scrabble_Tile_D.jpg",
+  E: "Scrabble_Tile_E.jpg",
+  F: "Scrabble_Tile_F.jpg",
+  G: "Scrabble_Tile_G.jpg",
+  H: "Scrabble_Tile_H.jpg",
+  I: "Scrabble_Tile_I.jpg",
+  J: "Scrabble_Tile_J.jpg",
+  K: "Scrabble_Tile_K.jpg",
+  L: "Scrabble_Tile_L.jpg",
+  M: "Scrabble_Tile_M.jpg",
+  N: "Scrabble_Tile_N.jpg",
+  O: "Scrabble_Tile_O.jpg",
+  P: "Scrabble_Tile_P.jpg",
+  Q: "Scrabble_Tile_Q.jpg",
+  R: "Scrabble_Tile_R.jpg",
+  S: "Scrabble_Tile_S.jpg",
+  T: "Scrabble_Tile_T.jpg",
+  U: "Scrabble_Tile_U.jpg",
+  V: "Scrabble_Tile_V.jpg",
+  W: "Scrabble_Tile_W.jpg",
+  X: "Scrabble_Tile_X.jpg",
+  Y: "Scrabble_Tile_Y.jpg",
+  Z: "Scrabble_Tile_Z.jpg",
+  _: "Scrabble_Tile_Blank.jpg",
+};
+
+// dictionary
+var dictarray = {};
+
+$(function () {
+  ButtonListeners();
+
+  //adding event listener for buttons
+  Setup();
+
+  //setting up scrabble the game
+  DragAndDrop();
+
+  //setting up drag and drop
+  Dictionary();
+
+  //setting up the dictonary for checking if current word is a word
+});
+
+// sets up the event listeners for buttons
+
+function ButtonListeners() {
+  //event listeners for the buttons
+  $("#submitButton").click(function () {
+    SubmitClick();
+  });
+
+  $("#resetButton").click(function () {
+    ResetAllLetters();
+  });
+
+  $("#newGame").click(function () {
+    ResetNewGame();
+  });
+}
+
+//  dictionary to array
+
+
+//https://johnresig.com/blog/dictionary-lookups-in-javascript/
+function Dictionary() {
+  //request the text dictionary
+  $.get(
+    "https:raw.githubusercontent.com/BMichaud7/BMichaud7.github.io/master/dict.txt",
+    function (txt, status) {
+      if (status == "success") {
+        // Get an array of all the words
+        var words = txt.split("\n");
+
+        //add word to dict
+        for (var i = 0; i < words.length; i++) {
+          dictarray[words[i]] = true;
+        }
+      }
+    }
+  );
+}
+
+// sets up drag and drop 
+//https://api.jqueryui.com/droppable/#event-drop
+function DragAndDrop() {
+  $(".selector").draggable({
+    delay: 100,
+    containment: "body",
+    revert: "invalid",
+
+    //reverts to hand if not placed on board
+    stack: ".selector",
+
+    //keeps draggable on top of all of the elements
+    drag: function (event, ui) {
+      if (
+        $(this).parent().attr("name") == "_" &&
+        $(this).parent().parent().attr("id") == "board"
+      ) {
+        $(this).parent().removeAttr("name");
+        $(this).attr("id", "_");
+        $(this).attr("src", "Scrabble_Tiles/" + GetImage("_"));
+      }
+    },
+  });
+//https://api.jqueryui.com/droppable/#event-drop
+  $(".spot").droppable({
+    //droppable event for the spots on board
+    tolerance: "intersect",
+    drop: function (event, ui) {
+      //drop event
+      $(ui.draggable)
+        .detach()
+        .css({ top: 0, left: 0 })
+        .appendTo("#" + this.id);
+      $("#" + this.id).droppable("disable");
+      MakespotsDroppable();
+      //resets spots on board that have no tiles and are no longer droppable
+      if (ui.draggable.attr("id") == "_") {
+        PromptFor_(ui.draggable);
+      }
+    },
+  });
+//https://api.jqueryui.com/droppable/#event-drop
+  $("#hand").droppable({
+    //making hand droppable
+    drop: function (event, ui) {
+      $(ui.draggable)
+        .detach()
+        .css({ top: 0, left: 0 })
+        .appendTo("#" + ui.draggable.attr("name"));
+      //code from Barry Pitman on stackoverflow.
+      MakespotsDroppable();
+    },
+  });
+}
+
+// Prompts the user letter thay want to use for a _ tile. Keeps asking until user enters A-Z
+function PromptFor_(tile) {
+  do {
+    tileRequested = prompt("Enter a letter for the _ Title");
+
+    if (
+      tileRequested != null &&
+      tileRequested.length == 1 &&
+      tileRequested.match(/[a-zA-Z]+/g)
+    ) {
+      tileRequested = tileRequested.toUpperCase();
+      $(tile).parent().attr("name", "_");
+      //used for resetting tiles
+      $(tile).attr("src", "Scrabble_Tiles/" + GetImage(tileRequested.trim()));
+      $(tile).attr("id", tileRequested);
+      //changing _ tile to requested tile
+    }
+  } while (
+    tileRequested == null ||
+    tileRequested.length > 1 ||
+    tileRequested.match(/[a-zA-Z]+/g) == null
+  );
+
+  //looping while user does not enter a valid response. This means the user needs to enter just A-Z
+}
+
+// removes _ tile from board
+function Remove_() {
+  $("#board div").each(function (index) {
+    //looping through each div in #board div
+    if ($(this).attr("name")) {
+      //checking if div has a name attribute
+      $(this).removeAttr("name");
+      //removing attribute from div
+    }
+  });
+}
+
+// Sets up ScrabbleTiles object and gets tles to add to the hand
+
 //This gets images of the inputed letter
 function GetImage(letter) {
   if (letter == "A") {
@@ -442,13 +442,13 @@ function GetRandom() {
   var tileNumber = Math.floor(Math.random() * remaining) + 1;
   //random num between 1 and remaining number of tiles
 
-  for (var key in ScrabbleTiles) {
-    tileNumber = tileNumber - ScrabbleTiles[key].number_remaining;
+  for (var letter in ScrabbleTiles) {
+    tileNumber = tileNumber - ScrabbleTiles[letter].number_remaining;
 
-    if (tileNumber <= 0 && key != null) {
-      ScrabbleTiles[key].number_remaining -= 1;
+    if (tileNumber <= 0 && letter != null) {
+      ScrabbleTiles[letter].number_remaining -= 1;
       //subtract the tile used from number_remaining
-      return key;
+      return letter;
     }
   }
 }
@@ -470,7 +470,7 @@ function SubmitClick() {
   var word = GetWord();
   var scoreofWord = 0;
 
-  if (!IsValidWord(word)) {
+  if (!validWord(word)) {
     //resets letters if invalid word
     ResetAllLetters();
     $("#scoreofWord").text(0);
@@ -512,7 +512,7 @@ function GetWord() {
 }
 
 // checks the dictionary if word is valid
-function IsValidWord(word) {
+function validWord(word) {
   return dictarray[word] ? true : false;
 }
 
@@ -586,7 +586,7 @@ function calcscore() {
       scoreofWord += ScrabbleTiles[id].value;
     }
   });
-
+  //double word tile
   if (isDoublescoreofWord) {
     scoreofWord = scoreofWord * 2;
   }
@@ -595,7 +595,7 @@ function calcscore() {
 }
 
 // starts a new game and resets current game
-function NewGame() {
+function ResetNewGame() {
   if ($("#gameOver")) {
     $("#gameOver").remove();
   }
